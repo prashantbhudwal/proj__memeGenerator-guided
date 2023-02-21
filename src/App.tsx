d1f3c6a029b664373a07771fb780e5eab1e7ca38
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-21 09:35:13 +0530

    Add Header component boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Global } from "@emotion/react";
+import Header from "./Components/Header";
 import globalStyles from "./globalStyles";
 
 export default function App() {
   return (
     <>
       <Global styles={globalStyles} />
+      <Header />
       The App Works
     </>
   );
---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const StyledHeader = styled.div`
+  background-color: #5e5252;
+  height: 75px;
+`;
+
+const Header = () => {
+  return <StyledHeader />;
+};
+
+export default Header;
